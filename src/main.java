2afacaad40bcb1ce4ca20adc1a3364fcf68c031c
Leonardo Lucas <leonardodlucasbri1@gmail.com>
2025-04-02 18:28:13 -0300

    adicionando remover e deslocaesquerda

--- src/main.java
+++ src/main.java
@@ -1,15 +1,48 @@
 package src;
+import java.util.Scanner;
+    public class main {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            Lista lista = new Lista();
+            
 
-public class main {
-    public static void main(String[] args) {
-        System.out.println("CONTATOS - JAVA");
+            while(true){
+                System.out.println("Menu");
+                System.out.println("1 - Cadastrar Aluno");
+                System.out.println("2 - Listar Alunos");
+                System.out.println("3 - Excluir Aluno");
+                System.out.println("4 - Procurar Aluno");
+                System.out.println("5 - Sair");
+                System.out.println("digite a opção desejada: ");
+                int opcao = input.nextInt();
 
-        Alunos alunotest = new Alunos("Juazeirinho do norte", 5446340);
+                switch(opcao){
+                    case 1:
+                        System.out.println("Digite o nome do aluno: ");
+                        String nome = input.next();
+                        input.nextLine();
+
+                        System.out.println("Digite o rgm  do aluno: : ");
+                        int rgm = input.nextInt();
+                        input.nextLine();
+                        Alunos aluno = new Alunos(nome, rgm);
+                        lista.insereAlunos(aluno);
+                        System.out.println("Aluno Cadastrado com sucesso!");
+                        break;
+                    case 2:
+                        lista.listaralunos();
+                        break;
+                    case 3:
+                        System.out.println("Digite o RGM do aluno que deseja remover: ");
+                        int RGM = input.nextInt();
+                        lista.remover(RGM);
+                        System.out.println("aluno removido");
+                        System.out.println("Lista atual: ");
+                        lista.listaralunos();
+                        break;
+                      
+                }
+            }
+                } 
+    }               
     
-        alunotest.disciplina = new Disciplinas();
-        alunotest.disciplina.nome_disciplina = "Ciencia da computacao";
-        alunotest.disciplina.nota = 7.0f;
-
-        System.out.println(alunotest.ShowAlunos());
-    }
-}

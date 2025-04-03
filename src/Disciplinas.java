cf7b59ac323b7d5251598c457400136a51ccd4b4
Maximus Feitoza Lira Cunha <151573378+MaxFeitoza@users.noreply.github.com>
2025-04-03 14:41:37 -0300

    lista encadeada + caso 4 + caso 5

--- src/Disciplinas.java
+++ src/Disciplinas.java
@@ -1,16 +1,34 @@
 package src;
 
-class Disciplinas{
+class Disciplinas<T>{
     String nome_disciplina;
     float nota;
-    Disciplinas prox;
+    private ListaEncadeada<T> inicio;
 
 
     void disciplinas(String nome_disciplina, float nota){
         this.nome_disciplina = nome_disciplina;
         this.nota = nota;
-        this.prox = null;
+        this.inicio = null;
+    }
+
+    void AdicionaDisciplina(T elemento){
+        ListaEncadeada<T> No = new ListaEncadeada<T>(elemento);  //Criacao do No
+        this.inicio = No; //Apontando o "inicio" para o "No"
+        
+    }
+
+    public String getNome_disciplina() {
+        return nome_disciplina;
+    }
 
 
+    public void setNome_disciplina(String nome_disciplina) {
+        this.nome_disciplina = nome_disciplina;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplinas [nome_disciplina=" + nome_disciplina + ", nota=" + nota + ", inicio=" + inicio + "]";
     }
 }
--- src/Lista.java
+++ src/Lista.java
@@ -1,10 +1,11 @@
 package src;
 
+import javax.print.DocFlavor.STRING;
 
 public class Lista{
     private static final int MAX = 60;
-    private Alunos[] alunos ;
-    private int ultPosicao = -1;
+    public Alunos[] alunos ;
+    public int tamanho_lista = -1;
     //inicializa a lista
     public Lista() {
         alunos = new Alunos[MAX];
@@ -12,20 +13,20 @@ public class Lista{
 
 
     public boolean listaVazia(){
-        return (ultPosicao == -1);
+        return tamanho_lista == -1;
     }
     public boolean listaCheia(){
-        return (ultPosicao == MAX - 1);
+        return tamanho_lista == MAX - 1;
     }
 
     public void deslocaDireita(int posicao){
-        for (int i = ultPosicao + 1; i > posicao; i--){
+        for (int i = tamanho_lista + 1; i > posicao; i--){
             alunos[i] = alunos[i - 1];
         }
 
     }
     public void deslocaEsquerda(int posicao){
-        for(int i = posicao; i < (ultPosicao - 1); i++){
+        for(int i = posicao; i < tamanho_lista - 1; i++){
             alunos[i] = alunos[i + 1];
         }
     }
@@ -39,7 +40,7 @@ public class Lista{
 
         // verifica se a posicao é menor ou igual que o ultimo Alunos e se o RGM do ultimo Alunos é menor  que o RGM do Alunos novo
         int posicao = 0;
-        while (posicao <= ultPosicao && (alunos[posicao].getRgm()) < (Alunos.getRgm())) {
+        while (posicao <= tamanho_lista && (alunos[posicao].getRgm()) < (Alunos.getRgm())) {
             posicao++;
         }
 
@@ -48,61 +49,46 @@ public class Lista{
 
         // coloca o Alunos na lista de alunos
         alunos[posicao] = Alunos;
-        ultPosicao++;
+        tamanho_lista++;
         return true;
     }
     
     public void listaralunos() {
         if (listaVazia()) {
             System.out.println("Nenhum Alunos cadastrado!");
-            return;
         }
         // percorre a lista mostrando os alunos
-        for (int i = 0; i <= ultPosicao; i++) {
+        for (int i = 0; i <= tamanho_lista; i++) {
             System.out.println("Alunos: " + alunos[i].getNome() + " | RGM: " + alunos[i].getRgm());
         }
     }
-    public boolean remover (int rgm){
-        if(listaVazia()){
-
-            System.out.println("Não existem alunos cadastrados!");
+    public boolean remover (int rgm){    
+        int posicao = -1;
+        for(int i = 0; i < tamanho_lista; i++){
+            if(rgm == alunos[i].getRgm()){
+                System.out.println(alunos[i].getRgm());
+                posicao = i;
+                deslocaEsquerda(3);
+                break;
+            }
+        }
+        if (posicao == -1){
+            System.out.println("Aluno não identificado! ");
             return false;
         }
-            
-        else{
-            int posicao = -1;
-            for(int i = 0; i < ultPosicao; i++){
-                if(rgm == alunos[i].getRgm()){
-                    System.out.println(alunos[i].getRgm());
-                    posicao = i;
-                    break;
-                }
-            }
-            if (posicao == -1){
-                System.out.println("Aluno não identificado! ");
-                return false;
-            }
-            /*
-                Primeiro deslocaEsquerda: move todos os elementos após a posição para esquerda
-
-                Depois limpamos alunos[ultPosicao] (que agora está duplicado)
-
-                Finalmente decrementamos ultPosicao
-             */
-            deslocaEsquerda(posicao);
-            ultPosicao--;
-            alunos[ultPosicao] = null;
-            return true;
-
-        }
-
-
-    } 
-
-        
-    
-            
+        alunos[tamanho_lista] = null;
+        tamanho_lista--;
+        return true;
     }
+    public String procurar(int rgm){
+        int index;
+        for(index = 0; index < 60; index += 1){
+            if(rgm == alunos[index].getRgm()){
+                return "ALUNO ENCONTRADO\n"+"Nome aluno: "+ alunos[index].getNome() + " Rgm: "+alunos[index].getRgm();
+            }
+        }
+        return "Aluno com esse rgm ("+Integer.toString(rgm) +") nao existe!";
+    }     
+}
 
 
-    
